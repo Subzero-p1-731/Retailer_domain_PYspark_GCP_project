01dae376b1b09bfba4a9a75f63c2ee527bb38603
subzero-p1-731 <subzero.p1.731@gmail.com>
2026-02-07 11:30:37 +0530

    updated the pyspark files for correct gcs_path

--- gcp-retailer-project/data/INGESTION/customerReviews_API.py
+++ gcp-retailer-project/data/INGESTION/customerReviews_API.py
@@ -8,8 +8,8 @@ from google.cloud import storage
 # Initialize Spark Session
 spark = SparkSession.builder.appName("CustomerReviewsAPI").getOrCreate()
 
-# API Endpoint
-API_URL = "https://67e51d5418194932a5849592.mockapi.io/retailer/reviews"
+# API EndPoint
+API_URL = "https://6975e498c0c36a2a994fb2f3.mockapi.io/retailer_projects/reviews"
 
 # Step 1: Fetch data from API
 response = requests.get(API_URL)
@@ -18,14 +18,14 @@ if response.status_code == 200:
     data = response.json()
     print(f"✅ Successfully fetched {len(data)} records.")
 else:
-    print(f"❌ Failed to fetch data. Status Code: {response.status_code}")
+    print(f"❌ Failed to fetch data. Status code : {response.status_code}")
     exit()
     
 # Step 2: Convert API Data to Pandas DataFrame
 df_pandas = pd.DataFrame(data)
 
-# Step 3: Get Current Date for File Naming
-today = datetime.datetime.today().strftime('%Y%m%d')  # Format: YYYYMMDD
+# Step 3: Get Current Date from File Naming
+today = datetime.date.today().strftime('%Y%m%d') # Format: YYYYMMDD
 
 # Step 4: Define File Paths with Date
 local_parquet_file = f"/tmp/customer_reviews_{today}.parquet"
@@ -35,10 +35,11 @@ GCS_PATH = f"landing/customer_reviews/customer_reviews_{today}.parquet"
 # Step 5: Save Pandas DataFrame as Parquet Locally
 df_pandas.to_parquet(local_parquet_file, index=False)
 
-# Step 6: Upload Parquet File to GCS
+# Step 6: Upload  Parquet File to GCS
 storage_client = storage.Client()
 bucket = storage_client.bucket(GCS_BUCKET)
 blob = bucket.blob(GCS_PATH)
 blob.upload_from_filename(local_parquet_file)
 
-print(f"✅ Data successfully written to gs://{GCS_BUCKET}/{GCS_PATH}")
+
+print(f"✅ Data Successfully written to gs://{GCS_BUCKET}/{GCS_PATH}")
